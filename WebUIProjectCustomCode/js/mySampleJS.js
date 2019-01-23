207657e8ed6e9d7c9bc6917e80f95e6c671a4886
ashishr <ashishr@AP-WFT-WIN13.NONPROD.HCLPNP.COM>
2019-01-23 04:23:46 -0800

    Adding new project to test custom code java and js custom code tests

---
+++ WebUIProjectCustomCode/js/mySampleJS.js
@@ -0,0 +1,11 @@
+function ClickOnGoogleEditText(){
+    console.log("Calling ClickOnGoogleEditText function");
+    document.getElementsByName("q")[0].click();
+}
+
+function EnterValueRationalInEditBox(var1){
+    console.log("Calling EnterValueRationalInEditBox function");
+    var2 = var1;
+    document.getElementsByName("q")[0].value = var2;
+}
+
